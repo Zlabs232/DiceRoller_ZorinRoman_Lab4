fe7d0f601dd6cd450311c829aa8e2ea471b9364b
Роман Зорин <sgena170315@gmail.com>
2025-11-05 01:13:14 +0300

    Работа с вариантами картинок

--- app/src/main/java/com/zoirn/diceroller_zorinroman_lab4/MainActivity.kt
+++ app/src/main/java/com/zoirn/diceroller_zorinroman_lab4/MainActivity.kt
@@ -16,7 +16,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.zoirn.diceroller_zorinroman_lab4.ui.theme.DiceRoller_ZorinRoman_Lab4Theme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
 
 
@@ -34,23 +40,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
     Column (
         modifier = Modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(R.drawable.dice_1),
-            contentDescription = "1"
+            contentDescription = result.toString()
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { }){
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
+
     }
 }
 
 @Preview
 @Composable
 fun DiceRollerApp(){
-    DiceWithButtonAndImage(modifier = Modifier)
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
 }
